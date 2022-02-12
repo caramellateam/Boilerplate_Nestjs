6c021e79b1d68998d6dd2bed04576bd7acf6292e
shellcodesniper <shellcodesniper@icloud.com>
2022-02-12 20:13:03 +0900

    [ NF ] HUSKY

---
+++ @types/passport/index.d.ts
@@ -0,0 +1,82 @@
+import TbMeMemA from '@src/users/entities/TbMeMemA.entity';
+
+declare global {
+  namespace Express {
+    // tslint:disable-next-line:no-empty-interface
+    export interface AuthInfo { }
+    // tslint:disable-next-line:no-empty-interface
+    export interface User {
+      // memNo: TbMeMemA['tmmaMemno'],
+      // gmemNo: TbMeMemA['tmmxGmemno'],
+      // isAdmin: boolean,
+      // NOTE override JWT USER INFO
+      iat?: number,
+      exp?: number,
+      iss?: string,
+    }
+
+    export interface Request {
+      authInfo?: AuthInfo | undefined;
+      user?: User | undefined;
+
+      // These declarations are merged into express's Request type
+      login(user: User, done: (err: any) => void): void;
+      login(user: User, options: any, done: (err: any) => void): void;
+      logIn(user: User, done: (err: any) => void): void;
+      logIn(user: User, options: any, done: (err: any) => void): void;
+
+      logout(): void;
+      logOut(): void;
+
+      isAuthenticated(): this is AuthenticatedRequest;
+      isUnauthenticated(): this is UnauthenticatedRequest;
+    }
+
+    export interface AuthenticatedRequest extends Request {
+      user: User;
+    }
+
+    export interface UnauthenticatedRequest extends Request {
+      user?: undefined;
+    }
+  }
+}
+declare namespace Express {
+  // tslint:disable-next-line:no-empty-interface
+  export interface AuthInfo { }
+  // tslint:disable-next-line:no-empty-interface
+  export interface User {
+    // memNo: TbMeMemA['tmmaMemno'],
+    // gmemNo: TbMeMemA['tmmxGmemno'],
+    // isAdmin: boolean,
+    // NOTE override JWT USER INFO
+    iat?: number,
+    exp?: number,
+    iss?: string,
+  }
+
+  export interface Request {
+    authInfo?: AuthInfo | undefined;
+    user?: User | undefined;
+
+    // These declarations are merged into express's Request type
+    login(user: User, done: (err: any) => void): void;
+    login(user: User, options: any, done: (err: any) => void): void;
+    logIn(user: User, done: (err: any) => void): void;
+    logIn(user: User, options: any, done: (err: any) => void): void;
+
+    logout(): void;
+    logOut(): void;
+
+    isAuthenticated(): this is AuthenticatedRequest;
+    isUnauthenticated(): this is UnauthenticatedRequest;
+  }
+
+  export interface AuthenticatedRequest extends Request {
+    user: User;
+  }
+
+  export interface UnauthenticatedRequest extends Request {
+    user?: undefined;
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core';
+import bodyParser from 'body-parser';
+
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.use(bodyParser.json({ limit: '50mb' }));
+  app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
+
   await app.listen(3000);
 }
+// NOTE 앱이 시작되는 부분
 bootstrap();
